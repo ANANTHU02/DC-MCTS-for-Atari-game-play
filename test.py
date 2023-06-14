4f7a7d7993b9b571d288bdb4973105cd5d340e65
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2023-06-14 17:00:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,12 +10,11 @@ def test(env, agent, num_episodes):
     for episode in range(num_episodes):
         state = env.reset()
         done = False
-        total_reward = 0  # Track the total reward for the episode
+        total_reward = 0  
         while not done:
             action = agent.select_action(state)
             next_state, reward, done, _ = env.step(action)
             state = next_state
-            #env.render()  # Render the environment
             total_reward += reward
             if done:
                 print("Episode {}: Total Reward = {}".format(episode + 1, total_reward))
@@ -29,8 +28,4 @@ for episode in range(num_episodes):
         action = agent.select_action(state)
         next_state, reward, done, _ = env.step(action)
         state = next_state
-        #print(reward,action,state)
-        #env.render()  # Render the environment to see the game
-
-#env.close()  # Close the environment after testing
 test(env, agent, num_episodes)
