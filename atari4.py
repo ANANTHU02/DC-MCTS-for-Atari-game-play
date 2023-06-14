c269921ce2809842f611728fc99310cba0343945
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2023-06-14 14:34:44 +0530

    Update atari4.py

--- atari4.py
+++ atari4.py
@@ -7,10 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Rs8N2pUeP9_njIaXzJ3EyNLraKbAZKyp
 """
 
-import numpy as np
-import torch
-import gym
-
 !pip install gym[atari]
 !pip install autorom[accept-rom-license]
 
